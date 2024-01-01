7cd07780780035a832c47c80dc0381e166041344
RainBowT <rainbowt0506@gmail.com>
2024-01-01 18:20:24 +0800

    feat: Create a Bokeh plot with Google Earth Terrain view
    - Utilized Bokeh's gmap module to create a plot with Google Maps integration
    - Configured the Google map with specified latitude, longitude, map type (terrain), and zoom level
    - Saved the plot as "Terrain.html"

---
+++ Terrain.py
@@ -0,0 +1,27 @@
+# importing the required modules
+from bokeh.plotting import gmap
+from bokeh.models import GMapOptions
+from bokeh.io import output_file, show
+
+from Constant import API_Key_Google_Map
+
+# file to save the model
+output_file("Terrain.html")
+
+# configuring the Google map
+lat = 30.3165
+lng = 78.0322
+map_type = "terrain"
+zoom = 12
+google_map_options = GMapOptions(lat = lat,
+								lng = lng,
+								map_type = map_type,
+								zoom = zoom)
+
+title = "Dehradun"
+google_map = gmap(API_Key_Google_Map,
+				google_map_options,
+				title = title)
+
+# displaying the model
+show(google_map)
